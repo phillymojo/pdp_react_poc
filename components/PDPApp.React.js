c14dfaae27a7f2afe93b8e734cadf13b33001be9
Morgan Bonar <mbonar@gmail.com>
2015-12-14 22:54:02 -0800

    added css files and starting to expose pdp data in the view

--- components/PDPApp.React.js
+++ components/PDPApp.React.js
@@ -2,24 +2,33 @@ var React = require('react');
 
 var PDPApp = React.createClass({
 	getInitialState: function(){
-		return {};
+		return {displayName: ''};
 	},
 
 	componentDidMount: function(){ 
 		//Fetch the PDP data via an ajax call
 		$.ajax({
+			context: this,
 			url: '//store.nike.com/html-services/templateData/pdpData?action=getPage&path=/us/en_us/pd/air-max-95-sneakerboot-boot/pid-10338627/pgid-10348285&productId=10338627&productGroupId=10348285&catalogId=1&country=US&lang_locale=en_US',
 			dataType: 'jsonp'
 		}).done(function(res){
-			console.log(res);
+			this.setState(res.response.pdpData);
 		});
 
 		
 	},
 
+	createMarkup: function(markup) { return {__html: markup} },
 
 	render: function(){
-		return <div className="PDPApp_container">Hey {this.props.data.title}</div>;
+		return (
+			<div className="PDPApp_container">
+				<div>{this.state.productTitle}</div>
+				<div>{this.state.productSubTitle}</div>
+				<div>{this.state.localPrice}</div>
+				<div dangerouslySetInnerHTML={this.createMarkup(this.state.colorwayGeneralMessage)} />
+			</div>
+		)
 	}
 });
 
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -15,24 +15,33 @@ var React = require('react');
 
 var PDPApp = React.createClass({displayName: "PDPApp",
 	getInitialState: function(){
-		return {};
+		return {displayName: ''};
 	},
 
 	componentDidMount: function(){ 
 		//Fetch the PDP data via an ajax call
 		$.ajax({
+			context: this,
 			url: '//store.nike.com/html-services/templateData/pdpData?action=getPage&path=/us/en_us/pd/air-max-95-sneakerboot-boot/pid-10338627/pgid-10348285&productId=10338627&productGroupId=10348285&catalogId=1&country=US&lang_locale=en_US',
 			dataType: 'jsonp'
 		}).done(function(res){
-			console.log(res);
+			this.setState(res.response.pdpData);
 		});
 
 		
 	},
 
+	createMarkup: function(markup) { return {__html: markup} },
 
 	render: function(){
-		return React.createElement("div", {className: "PDPApp_container"}, "Hey ", this.props.data.title);
+		return (
+			React.createElement("div", {className: "PDPApp_container"}, 
+				React.createElement("div", null, this.state.productTitle), 
+				React.createElement("div", null, this.state.productSubTitle), 
+				React.createElement("div", null, this.state.localPrice), 
+				React.createElement("div", {dangerouslySetInnerHTML: this.createMarkup(this.state.colorwayGeneralMessage)})
+			)
+		)
 	}
 });
 
